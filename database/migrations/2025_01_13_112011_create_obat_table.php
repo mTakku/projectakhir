157f694973f353efdaf70803f117f8fac9c5fd96
Takku <farelvivo06@gmail.com>
2025-01-13 22:50:09 +0700

    databse new

--- database/migrations/2025_01_13_112011_create_obat_table.php
+++ database/migrations/2025_01_13_112011_create_obat_table.php
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('obat');
+        Schema::dropIfExists('obats');
     }
 };
--- database/migrations/2025_01_13_112027_create_hasilpemeriksaan_table.php
+++ database/migrations/2025_01_13_112027_create_hasilpemeriksaan_table.php
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('hasilpemeriksaan');
+        Schema::dropIfExists('hasilpemeriksaans');
     }
 };
